5498da441a3f6628e7eca0bc048356685e2ac563
netosaurio11 <neto1014@gmail.com>
2021-02-04 21:14:57 -0600

    DAY23: Project 3. Custom modifiers

--- Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -7,6 +7,46 @@
 
 import SwiftUI
 
+// MARK: Custom modifiers - Title
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .padding()
+            .background(Color.blue)
+            .clipShape(RoundedRectangle(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        self.modifier(Title())
+    }
+}
+
+// MARK: Custom modifiers - Watermark
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundColor(.white)
+                .padding(5)
+                .background(Color.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        self.modifier(Watermark(text: text))
+    }
+}
+
 struct CapsuleText: View {
     var text: String
     
@@ -20,12 +60,9 @@ struct CapsuleText: View {
 }
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                .foregroundColor(.white)
-            CapsuleText(text: "Second")
-                .foregroundColor(.yellow)
-        }
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
     }
 }
 
