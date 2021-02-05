5b997a0f3c037ecdffdcf666c27d0cc47240c9b9
netosaurio11 <neto1014@gmail.com>
2021-02-04 20:48:46 -0600

    DAY23: Project 3. Conditional modifiers

--- Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useRedText = false
+
     var body: some View {
         Button("Hello World") {
-            print(type(of: self.body))
+            // flip the Boolean between true and false
+            self.useRedText.toggle()
         }
-        .frame(width: 200, height: 200)
-        .background(Color.red)
+        .foregroundColor(useRedText ? .red : .blue)
     }
 }
 
