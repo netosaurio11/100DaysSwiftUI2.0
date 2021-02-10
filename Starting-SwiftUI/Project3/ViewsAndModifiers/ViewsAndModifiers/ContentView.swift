f154a55a51f691cb4e9118ba6cb48bbb75bb988e
netosaurio11 <neto1014@gmail.com>
2021-02-09 19:31:48 -0600

    DAY23: Project 3. Custom containers

--- Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ Starting-SwiftUI/Project3/ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -58,11 +58,38 @@ struct CapsuleText: View {
             .clipShape(Capsule())
     }
 }
+
+// MARK: Custom containers
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0 ..< rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<self.columns, id: \.self) { column in
+                        self.content(row, column)
+                    }
+                }
+            }
+        }
+    }
+    
+    init(rows: Int, columns: Int, @ViewBuilder content: @escaping (Int, Int) -> Content) {
+        self.rows = rows
+        self.columns = columns
+        self.content = content
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        Color.blue
-            .frame(width: 300, height: 200)
-            .watermarked(with: "Hacking with Swift")
+        GridStack(rows: 4, columns: 4) { row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
+        }
     }
 }
 
