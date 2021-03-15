20ed711b13959609f2f15b59094674ac4b9b3282
netosaurio11 <neto1014@gmail.com>
2021-03-15 17:29:59 -0600

    DAY26. Training a model with Create ML

--- Expanding-Your-Skills/Project4/BetterRest/BetterRest/ContentView.swift
+++ Expanding-Your-Skills/Project4/BetterRest/BetterRest/ContentView.swift
@@ -12,16 +12,6 @@ struct ContentView: View {
     @State private var wakeUp = Date()
 
     var body: some View {
-        let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-        
-        let formatter = DateFormatter()
-        formatter.timeStyle = .short
-        let dateString = formatter.string(from: Date())
-        
-        let date = Calendar.current.date(from: components) ?? Date()
-        
         return DatePicker("Please enter a date", selection: $wakeUp)
     }
 }
