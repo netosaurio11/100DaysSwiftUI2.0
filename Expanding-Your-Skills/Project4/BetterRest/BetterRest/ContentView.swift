ef29d5989c2621db82990d4e21891ce39a635f37
netosaurio11 <neto1014@gmail.com>
2021-03-15 18:38:49 -0600

    DAY27. Cleaning up the user interface

--- Expanding-Your-Skills/Project4/BetterRest/BetterRest/ContentView.swift
+++ Expanding-Your-Skills/Project4/BetterRest/BetterRest/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-  @State private var wakeUp = Date()
+  @State private var wakeUp = defaultWakeTime
   @State private var sleepAmount = 8.0
   @State private var coffeeAmount = 1
 
@@ -18,23 +18,30 @@ struct ContentView: View {
 
   var body: some View {
     NavigationView {
-      VStack {
-        Text("When do you want to wake up?")
-          .font(.headline)
+      Form {
+        VStack(alignment: .leading, spacing: 0) {
+          Text("When do you want to wake up?")
+            .font(.headline)
 
-        DatePicker("Please entera time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-          .labelsHidden()
-
-        Text("Desired amount of sleep")
-          .font(.headline)
-        Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-          Text("\(sleepAmount, specifier:  "%g") hours")
+          DatePicker("Please entera time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+            .labelsHidden()
+            .datePickerStyle(WheelDatePickerStyle())
         }
 
-        Text("Daily coffe instake")
-          .font(.headline)
-        Stepper(value: $coffeeAmount, in: 1...20) {
-          coffeeAmount == 1 ? Text("1 cup") : Text("\(coffeeAmount) cups")
+        VStack(alignment: .leading, spacing: 0) {
+          Text("Desired amount of sleep")
+            .font(.headline)
+          Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
+            Text("\(sleepAmount, specifier:  "%g") hours")
+          }
+        }
+
+        VStack(alignment: .leading, spacing: 0) {
+          Text("Daily coffe instake")
+            .font(.headline)
+          Stepper(value: $coffeeAmount, in: 1...20) {
+            coffeeAmount == 1 ? Text("1 cup") : Text("\(coffeeAmount) cups")
+          }
         }
       }
       .navigationBarTitle("BetterRest")
@@ -47,6 +54,13 @@ struct ContentView: View {
         Alert(title: Text(alertTitle), message: Text(alertMessage))
       }
     }
+  }
+
+  static var defaultWakeTime: Date {
+    var components = DateComponents()
+    components.hour = 7
+    components.minute = 0
+    return Calendar.current.date(from: components) ?? Date()
   }
 
   func calculateBedTime() {
