23bd71f40619910bb2ba8a0a8ea0a18b4a651b17
netosaurio11 <neto1014@gmail.com>
2021-03-29 18:56:48 -0600

    DAY29. Introducing List (dynamic rows and sections)

---
+++ Expanding-Your-Skills/Project5/WordScramble/WordScramble/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 29/03/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+      List {
+          Section(header: Text("Section 1")) {
+              Text("Static row 1")
+              Text("Static row 2")
+          }
+
+          Section(header: Text("Section 2")) {
+              ForEach(0..<5) {
+                  Text("Dynamic row \($0)")
+              }
+          }
+
+          Section(header: Text("Section 3")) {
+              Text("Static row 3")
+              Text("Static row 4")
+          }
+      }
+      .listStyle(GroupedListStyle())
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Expanding-Your-Skills/Project5/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 29/03/21.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
