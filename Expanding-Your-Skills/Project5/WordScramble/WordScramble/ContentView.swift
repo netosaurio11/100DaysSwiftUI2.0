b990297cec8d55e3d99a055283e4b87eded4a43b
netosaurio11 <neto1014@gmail.com>
2021-03-29 19:07:05 -0600

    DAY29. Loading resources from your app bundle.

--- Expanding-Your-Skills/Project5/WordScramble/WordScramble/ContentView.swift
+++ Expanding-Your-Skills/Project5/WordScramble/WordScramble/ContentView.swift
@@ -11,9 +11,9 @@ struct ContentView: View {
   let people = ["Finn", "Leia", "Luke", "Rey"]
 
   var body: some View {
-      List(people, id: \.self) {
-          Text($0)
-      }
+    if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt"), let fileContents = try? String(contentsOf: fileURL) {
+      
+    }
   }
 }
 
