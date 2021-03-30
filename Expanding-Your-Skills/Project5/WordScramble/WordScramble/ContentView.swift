d77645922ea84b8596d49a835e2f3f837bafd865
netosaurio11 <neto1014@gmail.com>
2021-03-29 19:15:36 -0600

    DAY29. Working with strings(2 - checker)

--- Expanding-Your-Skills/Project5/WordScramble/WordScramble/ContentView.swift
+++ Expanding-Your-Skills/Project5/WordScramble/WordScramble/ContentView.swift
@@ -11,14 +11,11 @@ struct ContentView: View {
   let people = ["Finn", "Leia", "Luke", "Rey"]
 
   var body: some View {
-    let input = """
-                a
-                b
-                c
-                """
-    let letters = input.components(separatedBy: "\n")
-    let letter = letters.randomElement()
-    let trimmed = letter?.trimmingCharacters(in: .whitespacesAndNewlines)
+    let word = "swift"
+    let checker = UITextChecker()
+    let range = NSRange(location: 0, length: word.utf16.count)
+    let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+    let allGood = misspelledRange.location == NSNotFound
   }
 }
 
