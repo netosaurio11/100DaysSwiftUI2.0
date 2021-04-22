86e101ab02c6e9cd9b00a279f2c8b2d3517159de
netosaurio11 <neto1014@gmail.com>
2021-04-21 19:51:04 -0500

    DAY33. Project6: Building custom transitions using ViewModifier.

--- Expanding-Your-Skills/Project6/Animations/Animations/ContentView.swift
+++ Expanding-Your-Skills/Project6/Animations/Animations/ContentView.swift
@@ -23,9 +23,19 @@ struct ContentView: View {
         Rectangle()
           .fill(Color.red)
           .frame(width: 100, height: 100)
-          .transition(.asymmetric(insertion: .opacity, removal: .scale))
+          .transition(.pivot)
       }
     }
+  }
+}
+
+struct CornerRotateModifier: ViewModifier {
+  let amount: Double
+  let anchor: UnitPoint
+
+  func body(content: Content) -> some View {
+    content.rotationEffect(.degrees(amount), anchor: anchor)
+      .clipped()
   }
 }
 
@@ -33,4 +43,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension AnyTransition {
+  static var pivot: AnyTransition {
+    .modifier(active: CornerRotateModifier(amount: -90, anchor: .topLeading), identity: CornerRotateModifier(amount: 0, anchor: .topLeading))
+  }
 }
