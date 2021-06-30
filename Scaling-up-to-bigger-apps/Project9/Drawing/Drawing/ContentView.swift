44ddaebdf4f30b6ae47b081f66d8d483e99463a6
netosaurio11 <neto1014@gmail.com>
2021-06-29 22:16:19 -0500

    DAY 43. Paths vs shapes in SwiftUI

--- Scaling-up-to-bigger-apps/Project9/Drawing/Drawing/ContentView.swift
+++ Scaling-up-to-bigger-apps/Project9/Drawing/Drawing/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+struct Triangle: Shape {
+  func path(in rect: CGRect) -> Path {
+    var path = Path()
+
+    path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+    path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+    path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+    return path
+  }
+}
+
+struct Arc: Shape {
+  var startAngle: Angle
+  var endAngle: Angle
+  var clockwise: Bool
+
+  func path(in rect: CGRect) -> Path {
+    let rotationAdjustment = Angle.degrees(90)
+    let modifiedStart = startAngle - rotationAdjustment
+    let modifiedEnd = endAngle - rotationAdjustment
+
+    var path = Path()
+    path.addArc(center: CGPoint(x: rect.midX, y: rect.midY), radius: rect.width / 2, startAngle: modifiedStart, endAngle: modifiedEnd, clockwise: !clockwise)
+
+    return path
+  }
+}
+
 struct ContentView: View {
     var body: some View {
-      Path { path in
-        path.move(to: CGPoint(x: 150, y: 100))
-        path.addLine(to: CGPoint(x: 50, y: 300))
-        path.addLine(to: CGPoint(x: 250, y: 300))
-        path.addLine(to: CGPoint(x: 150, y: 100))
-      }
-      .stroke(Color.blue.opacity(0.25) , style: StrokeStyle(lineWidth: 10, lineCap: .round, lineJoin: .round))
+      Arc(startAngle: .degrees(0), endAngle: .degrees(180), clockwise: true)
+        .stroke(Color.blue, lineWidth: 10)
+        .frame(width: 300, height: 300)
     }
 }
 
