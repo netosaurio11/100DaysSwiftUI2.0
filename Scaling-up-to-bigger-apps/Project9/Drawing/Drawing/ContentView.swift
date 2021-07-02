081a98dde09e165163be52d213612ab96daf924b
netosaurio11 <neto1014@gmail.com>
2021-07-01 20:27:12 -0500

    DAY 44. Creative borders and fills using ImagePaint

--- Scaling-up-to-bigger-apps/Project9/Drawing/Drawing/ContentView.swift
+++ Scaling-up-to-bigger-apps/Project9/Drawing/Drawing/ContentView.swift
@@ -7,56 +7,13 @@
 
 import SwiftUI
 
-struct Flower: Shape {
-  // How much to move this petal away from the center
-  var petalOffset: Double = -20
-
-  // How wide to make each pedal
-  var petalWidth: Double = 100
-
-  func path(in rect: CGRect) -> Path {
-    // The path that will hold all petals
-    var path = Path()
-
-    // Coun from 0 to pi*2 , movin up pi/8 each time
-    for number in stride(from: 0, to: CGFloat.pi * 2, by: CGFloat.pi / 8) {
-      // rotate the petal by the current value of our loop
-      let rotation = CGAffineTransform(rotationAngle: number)
-
-      // move the petal to be at the center of our view
-      let position = rotation.concatenating(CGAffineTransform(translationX: rect.width / 2, y: rect.height / 2))
-
-      // create a path for this petal using our properties plus a fixed Y and height
-      let originalPetal = Path(ellipseIn: CGRect(x: CGFloat(petalOffset), y: 0, width: CGFloat(petalWidth), height: rect.width / 2))
-
-      // apply our rotation/position transformation to the petal
-      let rotatedPetal = originalPetal.applying(position)
-
-      // Add it to our main path
-      path.addPath(rotatedPetal)
-    }
-
-    return path
-  }
-}
-
 struct ContentView: View {
-  @State private var petalOffset = -20.0
-  @State private var petalWidth = 100.0
 
   var body: some View {
-    VStack {
-      Flower(petalOffset: petalOffset, petalWidth: petalWidth)
-        .fill(Color.red, style: FillStyle(eoFill: true))
+    Capsule()
+      .strokeBorder(ImagePaint(image: Image("Example"), scale: 0.1), lineWidth: 20)
+      .frame(width: 300, height: 200)
 
-      Text("Offset")
-      Slider(value: $petalOffset, in: -40...40)
-        .padding([.horizontal, .bottom])
-
-      Text("Width")
-      Slider(value: $petalWidth, in: 0...100)
-        .padding(.horizontal)
-    }
   }
 }
 
