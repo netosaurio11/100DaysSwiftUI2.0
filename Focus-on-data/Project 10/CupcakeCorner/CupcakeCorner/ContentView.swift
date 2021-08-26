94666d409e9b8ce6666928bb45dce5a6a059b493
netosaurio11 <neto1014@gmail.com>
2021-08-25 20:19:33 -0500

    DAY-49: Sending and receiving Codable data with URLSession and SwiftUI.

--- Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
+++ Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/ContentView.swift
@@ -7,29 +7,56 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
+struct Response: Codable {
+  var results: [Result]
+}
 
-  enum CodingKeys: CodingKey {
-    case name
-  }
-
-  required init(from decoder: Decoder) throws {
-    let container = try decoder.container(keyedBy: CodingKeys.self)
-    name = try container.decode(String.self, forKey: .name)
-  }
-
-    @Published var name = "Paul Hudson"
-
-  func encode(to encoder: Encoder) throws {
-    var container = encoder.container(keyedBy: CodingKeys.self)
-    try container.encode(name, forKey: .name)
-  }
+struct Result: Codable {
+  var trackId: Int
+  var trackName: String
+  var collectionName: String
 }
 
 struct ContentView: View {
+  @State private var results = [Result]()
+
   var body: some View {
-    Text("Hello, world!")
-      .padding()
+    List(results, id: \.trackId) { item in
+      VStack(alignment: .leading) {
+        Text(item.trackName)
+          .font(.headline)
+        Text(item.collectionName)
+      }
+    }
+    .onAppear(perform: loadData)
+  }
+
+  func loadData() {
+    // 1.Creating the URL we want to read.
+    guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else { return }
+
+    //2.Wrapping that in a URLRequest, which allows us to configure how the URL should be accessed.
+    let request = URLRequest(url: url)
+
+    //3.Create and start a networking task from that URL request.
+    URLSession.shared.dataTask(with: request) { data, response, error in
+      //4.Handle the result of that networking task.
+      if let data = data {
+        if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+          // we have good data – go back to the main thread
+          DispatchQueue.main.async {
+            // update our UI
+            self.results = decodedResponse.results
+          }
+
+          // everything is good, so we can exit
+          return
+        }
+      }
+
+      // if we're still here it means there was a problem
+      print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+    }.resume()
   }
 }
 
