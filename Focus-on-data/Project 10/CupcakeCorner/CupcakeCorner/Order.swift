fbf861163f1a17ad4cf03c8e63c0803028559086
netosaurio11 <neto1014@gmail.com>
2021-08-31 19:24:07 -0500

    DAY-51: Encoding an ObservableObject class.

--- Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/Order.swift
+++ Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/Order.swift
@@ -7,7 +7,12 @@
 
 import Foundation
 
-class Order: ObservableObject {
+class Order: ObservableObject, Codable {
+
+  enum CodingKeys: CodingKey {
+    case type, quantity, extraFrosting, addSprinkles, name, streetAddress, city, zip
+  }
+
   static let types = ["Vanilla", "Strawberry", "Chocolate", "Rainbow"]
 
   @Published var type = 0
@@ -30,12 +35,29 @@ class Order: ObservableObject {
   @Published var city = ""
   @Published var zip = ""
 
-  var hasValidAddress: Bool {
-      if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
-          return false
-      }
+  init() { }
 
-      return true
+  required init(from decoder: Decoder) throws {
+    let container = try decoder.container(keyedBy: CodingKeys.self)
+
+    type = try container.decode(Int.self, forKey: .type)
+    quantity = try container.decode(Int.self, forKey: .quantity)
+
+    extraFrosting = try container.decode(Bool.self, forKey: .extraFrosting)
+    addSprinkles = try container.decode(Bool.self, forKey: .addSprinkles)
+
+    name = try container.decode(String.self, forKey: .name)
+    streetAddress = try container.decode(String.self, forKey: .streetAddress)
+    city = try container.decode(String.self, forKey: .city)
+    zip = try container.decode(String.self, forKey: .zip)
+  }
+
+  var hasValidAddress: Bool {
+    if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+      return false
+    }
+
+    return true
   }
 
   var cost: Double {
@@ -47,14 +69,29 @@ class Order: ObservableObject {
 
     // $1/cake for extra frosting
     if extraFrosting {
-        cost += Double(quantity)
+      cost += Double(quantity)
     }
 
     // $0.50/cake for sprinkles
     if addSprinkles {
-        cost += Double(quantity) / 2
+      cost += Double(quantity) / 2
     }
 
     return cost
+  }
+
+  func encode(to encoder: Encoder) throws {
+    var container = encoder.container(keyedBy: CodingKeys.self)
+
+    try container.encode(type, forKey: .type)
+    try container.encode(quantity, forKey: .quantity)
+
+    try container.encode(extraFrosting, forKey: .extraFrosting)
+    try container.encode(addSprinkles, forKey: .addSprinkles)
+
+    try container.encode(name, forKey: .name)
+    try container.encode(streetAddress, forKey: .streetAddress)
+    try container.encode(city, forKey: .city)
+    try container.encode(zip, forKey: .zip)
   }
 }
