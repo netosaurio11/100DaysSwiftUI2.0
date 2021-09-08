2415687e91a6860c6d2c567797e0a2275d74d026
netosaurio11 <neto1014@gmail.com>
2021-09-07 19:57:12 -0500

    Challenge DAY-52. 2- If our call to placeOrder() fails show an informative alert for the user.

--- Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
+++ Focus-on-data/Project 10/CupcakeCorner/CupcakeCorner/CheckoutView.swift
@@ -11,6 +11,7 @@ struct CheckoutView: View {
   @ObservedObject var order: Order
 
   @State private var confirmationMessage = ""
+  @State private var alertTitle = ""
   @State private var showingConfirmation = false
 
 
@@ -35,7 +36,7 @@ struct CheckoutView: View {
     }
     .navigationBarTitle("Checkout", displayMode: .inline)
     .alert(isPresented: $showingConfirmation) {
-      Alert(title: Text("Thank you!"), message: Text(confirmationMessage), dismissButton: .default(Text("Ok")))
+      Alert(title: Text(alertTitle), message: Text(confirmationMessage), dismissButton: .default(Text("Ok")))
     }
   }
 
@@ -53,15 +54,22 @@ struct CheckoutView: View {
 
     URLSession.shared.dataTask(with: request) { data, response, error in
       guard let data = data else {
-        print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+        let errorDescription = error?.localizedDescription ?? "Try later"
+        self.alertTitle = "Error!"
+        self.confirmationMessage = "Something went wrong: \(errorDescription)"
+        self.showingConfirmation = true
         return
       }
 
       if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+        self.alertTitle = "Thank you!"
         self.confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
         self.showingConfirmation = true
       } else {
-        print("Invalid response from server")
+        let errorDescription = error?.localizedDescription ?? "Try later"
+        self.alertTitle = "Error!"
+        self.confirmationMessage = "Something went wrong: \(errorDescription)"
+        self.showingConfirmation = true
       }
     }.resume()
   }
