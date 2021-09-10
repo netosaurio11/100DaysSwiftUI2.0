f57f5dc3db296b4d0ce1b491be6ca20d3eb748f9
netosaurio11 <neto1014@gmail.com>
2021-09-09 20:45:00 -0500

    DAY-53. How to combine Core Data and SwiftUI

--- Focus-on-data/Project11/Bookworm/Bookworm/ContentView.swift
+++ Focus-on-data/Project11/Bookworm/Bookworm/ContentView.swift
@@ -6,24 +6,35 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ContentView: View {
-  @Environment(\.horizontalSizeClass) var sizeClass
+  @Environment(\.managedObjectContext) private var moc
 
+  @FetchRequest(entity: Student.entity(), sortDescriptors: [])
+  private var students: FetchedResults<Student>
 
   var body: some View {
-    if sizeClass == .compact {
-      return AnyView(VStack {
-        Text("Active size class:")
-        Text("COMPACT")
+    VStack {
+      List {
+        ForEach(students, id: \.id) { student in
+          Text(student.name ?? "Unknown")
+        }
       }
-      .font(.largeTitle))
-    } else {
-      return AnyView(HStack {
-        Text("Active size class:")
-        Text("REGULAR")
+
+      Button("Add") {
+        let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron"]
+        let lastNames = ["Granger", "Lovegood", "Potter", "Weasley"]
+
+        let chosenFirstName = firstNames.randomElement()!
+        let chosenLastName = lastNames.randomElement()!
+
+        let student = Student(context: self.moc)
+        student.id = UUID()
+        student.name = "\(chosenFirstName) \(chosenLastName)"
+
+        try? self.moc.save()
       }
-      .font(.largeTitle))
     }
   }
 }
@@ -31,24 +42,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
     ContentView()
-  }
-}
-
-struct PushButton: View {
-  let title: String
-  @Binding var isOn: Bool
-
-  var onColors = [Color.green, Color.blue ]
-  var offColors = [Color(white: 0.6), Color(white: 0.4)]
-
-  var body: some View {
-    Button(title) {
-      self.isOn.toggle()
-    }
-    .padding()
-    .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
-    .foregroundColor(.white)
-    .clipShape(Capsule())
-    .shadow(radius: isOn ? 0 : 5)
   }
 }
