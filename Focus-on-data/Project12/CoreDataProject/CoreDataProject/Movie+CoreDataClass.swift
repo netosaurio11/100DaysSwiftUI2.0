f5a6f676542692ad7bc888cd7a64487e4a861461
netosaurio11 <neto1014@gmail.com>
2021-10-07 20:14:35 -0500

    DAY-57. Creating NSManagedObject subclasses

---
+++ Focus-on-data/Project12/CoreDataProject/CoreDataProject/Movie+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Movie+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 07/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Movie)
+public class Movie: NSManagedObject {
+  
+}
