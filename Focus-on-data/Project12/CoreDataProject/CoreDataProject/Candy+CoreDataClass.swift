cf7cacf6d8fdb659a17cc29eb3b0b0951ddb3329
netosaurio11 <neto1014@gmail.com>
2021-10-12 21:54:04 -0500

    DAY-58. One-to-many relationships with Core Data, SwiftUI, and @FetchRequest

---
+++ Focus-on-data/Project12/CoreDataProject/CoreDataProject/Candy+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Candy+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 12/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Candy)
+public class Candy: NSManagedObject {
+
+}
---
+++ Focus-on-data/Project12/CoreDataProject/CoreDataProject/Country+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Country+CoreDataClass.swift
+//  CoreDataProject
+//
+//  Created by Ernesto Daniel Mejia Valdiviezo on 12/10/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Country)
+public class Country: NSManagedObject {
+
+}
