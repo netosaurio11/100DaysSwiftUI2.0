7031f94d7f055d68e3dc1d91b22f6cb787e80b77
netosaurio11 <neto1014@gmail.com>
2021-10-28 20:45:38 -0500

    DAY-59. CHALLENGE-DAY: 2. & 3. Accepting a predicate on the filtered list view.

--- Focus-on-data/Project12/CoreDataProject/CoreDataProject/ChallengeView.swift
+++ Focus-on-data/Project12/CoreDataProject/CoreDataProject/ChallengeView.swift
@@ -49,24 +49,48 @@ enum SortedDescription: String, CaseIterable {
   }
 }
 
+enum PredicateDescription: String, CaseIterable {
+  case equalsTo
+  case smallerThan
+  case greaterThan
+  case smallerOrEqualThan
+  case greaterOrEqualThan
+  case beginsWith
+  case beginsWithCaseSensitive
+
+  var description: String {
+    switch self {
+    case.equalsTo:
+      return "=="
+    case .smallerThan:
+      return "<"
+    case .greaterThan:
+      return ">"
+    case .smallerOrEqualThan:
+      return "<="
+    case .greaterOrEqualThan:
+      return ">="
+    case .beginsWith:
+      return "BEGINSWITH"
+    case .beginsWithCaseSensitive:
+      return "BEGINSWITH[c]"
+    }
+  }
+}
+
 struct ChallengeView: View {
   @Environment(\.managedObjectContext) var moc
   @State private var sortedDescription: SortedDescription = .firstNameAscending
+  @State private var predicate: PredicateDescription = .equalsTo
   @State private var letterFilter = "A"
 
   let lettersFilter: [String] = ["A", "J", "K", "O", "T", "L", "G", "C", "S"]
 
 
   var body: some View {
-    VStack {
-      Section(header: Text("Filtered list")) {
-        FilteredListView(filterKey: sortedDescription.filterKey, filterValue: letterFilter, descriptors: sortedDescription.descriptor) { (singer: Singer) in
-          Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
-        }
-      }
-
-      Section(header: Text("Filter configuration")) {
-        Form {
+    Group {
+      Form {
+        Section(header: Text("Filter configuration")) {
           // 1. Make it accept an array of NSSortDescriptor objects to get used in its fetch request.
           Picker("Sort description", selection: $sortedDescription) {
             ForEach(SortedDescription.allCases, id: \.self) { descriptor in
@@ -74,42 +98,56 @@ struct ChallengeView: View {
             }
           }
 
-          // 2. Make it accept a string parameter that controls which predicate is applied
           Picker("Letter filter", selection: $letterFilter) {
             ForEach(lettersFilter, id: \.self) { letter in
               Text(letter)
             }
           }
-        }
 
-        Button("Add Examples") {
-          let kendrick = Singer(context: self.moc)
-          kendrick.firstName = "Kendrick"
-          kendrick.lastName = "Lamar"
-
-          let ana = Singer(context: self.moc)
-          ana.firstName = "Ana"
-          ana.lastName = "Gabriel"
-
-          let jhay = Singer(context: self.moc)
-          jhay.firstName = "Jhay"
-          jhay.lastName = "Cortez"
-
-          let taylor = Singer(context: self.moc)
-          taylor.firstName = "Taylor"
-          taylor.lastName = "Swift"
-
-          let ed = Singer(context: self.moc)
-          ed.firstName = "Ed"
-          ed.lastName = "Sheeran"
-
-          let adele = Singer(context: self.moc)
-          adele.firstName = "Adele"
-          adele.lastName = "Adkins"
-
-          try? self.moc.save()
+          // 2. & 3. Modify the predicate string parameter to be an enum such as .beginsWith
+          Picker("Predicate", selection: $predicate) {
+            ForEach(PredicateDescription.allCases, id: \.self) { predicate in
+              Text(predicate.description)
+            }
+          }
         }
       }
+
+      Text("Filtered list")
+        .font(.headline)
+      FilteredListView(filterKey: sortedDescription.filterKey, filterValue: letterFilter, predicate: predicate.description, descriptors: sortedDescription.descriptor) { (singer: Singer) in
+        Text("\(singer.wrappedFirstName) \(singer.wrappedLastName)")
+      }
+      Spacer()
+      Button("Add Examples") {
+        let kendrick = Singer(context: self.moc)
+        kendrick.firstName = "Kendrick"
+        kendrick.lastName = "Lamar"
+
+        let ana = Singer(context: self.moc)
+        ana.firstName = "Ana"
+        ana.lastName = "Gabriel"
+
+        let jhay = Singer(context: self.moc)
+        jhay.firstName = "Jhay"
+        jhay.lastName = "Cortez"
+
+        let taylor = Singer(context: self.moc)
+        taylor.firstName = "Taylor"
+        taylor.lastName = "Swift"
+
+        let ed = Singer(context: self.moc)
+        ed.firstName = "Ed"
+        ed.lastName = "Sheeran"
+
+        let adele = Singer(context: self.moc)
+        adele.firstName = "Adele"
+        adele.lastName = "Adkins"
+
+        try? self.moc.save()
+      }
+
+      .navigationBarTitle("Challenge View", displayMode: .large)
     }
   }
 }
